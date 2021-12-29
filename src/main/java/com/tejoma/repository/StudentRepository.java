ffc120a748e0cfaaa8cad6dff971b0d960ae9ce8
venus <venusd95@gmail.com>
2021-12-30 01:25:35 +0530

    first commit

---
+++ src/main/java/com/tejoma/repository/StudentRepository.java
@@ -0,0 +1,10 @@
+package com.tejoma.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.tejoma.model.Student;
+@Repository
+public interface StudentRepository extends JpaRepository<Student, Long> {
+
+}
---
+++ src/test/java/com/tejoma/TejomaStudentAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tejoma;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TejomaStudentAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
